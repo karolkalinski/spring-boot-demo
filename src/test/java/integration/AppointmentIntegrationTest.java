1e76066ca3b148c6ba79e98579212d7c2170f1d5
Karol Kalinski <karol.kalinski@4finance.com>
2014-03-27 14:09:23 +0100

    classic integration rewritten as web mvc test

--- src/test/java/integration/AppointmentIntegrationTest.java
+++ src/test/java/integration/AppointmentIntegrationTest.java
@@ -1,35 +1,31 @@
 package integration;
 
 import demo.BootDemo;
-import demo.FirstController;
 import demo.domain.Appointment;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 import demo.domain.AppointmentRepository;
+import org.hamcrest.Matcher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.RestTemplates;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-import java.util.Collection;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
-import static org.hamcrest.Matchers.hasSize;
-
-
-
+@SuppressWarnings("SpringJavaAutowiredMembersInspection")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = BootDemo.class)
+@IntegrationTest
+@WebAppConfiguration
 public class AppointmentIntegrationTest {
     public static final Appointment APPOINTMENT = new Appointment("Stand up", "Mercury");
-    @Autowired
-    FirstController controller;
 
-    @Autowired
-    AppointmentRepository repository;
+    @Autowired private AppointmentRepository repository;
 
     @Test
     public void shouldReturnAllAppointments() {
@@ -37,10 +33,19 @@ public class AppointmentIntegrationTest {
         thereIsAppointmentInRepository();
 
         //when
-        Collection<Appointment> actual = controller.get();
+        ResponseEntity response = restClientAsksForAllApointments();
 
         //then
-        assertThat(actual , hasSize(1));
+        assertThat(response, isProper());
+    }
+
+    private Matcher<? super ResponseEntity> isProper() {
+        return notNullValue();
+    }
+
+    private ResponseEntity<String> restClientAsksForAllApointments() {
+        return RestTemplates.get().getForEntity(
+                "http://localhost:8080/appointments", String.class);
     }
 
     private void thereIsAppointmentInRepository() {
